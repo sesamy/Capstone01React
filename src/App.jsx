f675af9833c813128037d03771ac6478445a28bb
sesamy <amy.hori97@gmail.com>
2024-01-04 16:44:26 -0800

    close branch

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage token={token} />} />
         <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/:productId" element="Specific Product" />
