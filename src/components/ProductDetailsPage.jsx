535fe61068937d6d55c0dcc299cd103b052939d6
sesamy <amy.hori97@gmail.com>
2024-01-11 18:07:34 -0800

    feat create new cart if doesn't exist

--- src/components/ProductDetailsPage.jsx
+++ src/components/ProductDetailsPage.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchSingleProduct } from "../api/products.js";
 import { priceFormatter } from "../utils/helpers.js";
-import { addProductToCart, updateProductInCart } from "../api/cart.js";
+import { addProductToNewCart, updateProductInCart } from "../api/cart.js";
 
 export default function ProductDetailsPage() {
   const { productId } = useParams();
   const [product, setProduct] = useState([]);
   const [newAmount, setNewAmount] = useState([]);
+  const storedCartId = null;
+  const tempUserId = 1;
 
   useEffect(() => {
     async function getData() {
@@ -22,19 +24,34 @@ export default function ProductDetailsPage() {
   }, []);
 
   async function addToCartHandler(e) {
-    try {
-      e.preventDefault();
-      const dataObj = {
-        cartId: "5",
-        date: "2019-12-10",
-        productId: productId,
-        productQuantity: newAmount,
-      };
-      const data = await updateProductInCart(dataObj);
-      console.log(data);
-      return data;
-    } catch (err) {
-      console.error(err);
+    if (storedCartId) {
+      try {
+        e.preventDefault();
+        const dataObj = {
+          cartId: storedCartId,
+          date: "2019-12-10",
+          productId: productId,
+          productQuantity: newAmount,
+        };
+        const data = await updateProductInCart(dataObj);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      try {
+        e.preventDefault();
+        const dataObj = {
+          userId: tempUserId,
+          date: "2019-12-10",
+          productId: productId,
+          productQuantity: newAmount,
+        };
+        const data = await addProductToNewCart(dataObj);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
     }
   }
 
@@ -42,7 +59,7 @@ export default function ProductDetailsPage() {
     <div>
       <img src={product.image} className="product-details-image" />
       <p>{product.title}</p>
-      <p>{product.price}</p>
+      <p>{priceFormatter(product.price)}</p>
       <form onSubmit={addToCartHandler}>
         <input
           type="number"
